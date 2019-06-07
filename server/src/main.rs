eb4f5e4184764f3927184a2d7153ebe98fb5afc0
Lucas Salibian <lucas.salibian@gmail.com>
2019-06-06 19:53:43 -0700

    Basic config for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use log::info;
+
+#[derive(Clone, Debug)]
+enum AgentSource {
+    File,
+    Env,
+}
+
+#[derive(Clone, Debug)]
+struct Settings {
+    agent_source: AgentSource,
+    agents: Vec<String>,
+}
+
+fn get_settings() -> Settings {
+    let agent_source = match env::var("") {
+        Ok(var) => match &var as &str {
+            "file" => AgentSource::File,
+            "env" => AgentSource::Env,
+            _ => panic!("invalid parameter for agent source"),
+        }
+        Err(_) => AgentSource::Env,
+    };
+
+    Settings {
+        agent_source,
+        agents: Vec::new(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    info!("Loading settings...");
+    let _settings = get_settings();
 }
