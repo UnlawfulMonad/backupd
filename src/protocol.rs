0e6f8ae864d7742d60350d1d4fb197ee83d69592
Lucas Salibian <lucas.salibian@gmail.com>
2019-06-06 19:33:28 -0700

    Tests for protocol

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,12 +1,35 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct FileHeader {
-    path: String,
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Handshake {
+    pub version: u64,
+
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct FileHeader {
+    pub path: String,
+}
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Frame {
-    last: bool,
-    data: Vec<u8>,
+    pub last: bool,
+    pub data: Vec<u8>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rmp_serde::{
+        to_vec as pack,
+        from_slice as unpack,
+    };
+
+    #[test]
+    fn test_serialize_handshake() {
+        let hs = Handshake { version: 0 };
+        let hs_bin = pack(&hs).unwrap();
+        let hs_deserialized = unpack(&hs_bin[..]).unwrap();
+        assert_eq!(hs, hs_deserialized);
+    }
 }
