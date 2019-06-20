f2bfcbd44ec9783f9f6124038e3d75912d9a06d5
Lucas Salibian <lucas.salibian@gmail.com>
2019-06-20 13:24:54 -0700

    Added tests

---
+++ tests/slow_protocol.rs
@@ -0,0 +1,27 @@
+use backupd::Handshake;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+use std::net::{TcpListener, TcpStream};
+
+#[test]
+fn test_slow_handshake() {
+    let listen = TcpListener::bind("127.0.0.1:34215").expect("failed to create listener");
+    let t = thread::spawn(move || {
+        let mut stream = TcpStream::connect("127.0.0.1:34215").unwrap();
+        let handshake = Handshake{ version: 1, name: "name".into(), secret: "secret".into() };
+        let data = rmp_serde::to_vec(&handshake).unwrap();
+        stream.write_all(&data[..5]).unwrap();
+        thread::sleep(Duration::from_secs(1));
+
+        stream.write_all(&data[5..]).unwrap();
+    });
+
+    let (stream, _) = listen.accept().unwrap();
+    let handshake: Handshake = rmp_serde::from_read(stream).expect("Failed to read handshake");
+    assert_eq!(handshake.version, 1);
+    assert_eq!(handshake.name, "name");
+    assert_eq!(handshake.secret, "secret");
+
+    t.join().unwrap();
+}
